eb28cb107bef60f6efce52002747993d0d2b2529
Jason Luo <jasonl960523@gmail.com>
2017-08-20 11:27:57 +0800

    Fix the no-slug article

--- routes/api/articles.js
+++ routes/api/articles.js
@@ -8,7 +8,7 @@ var auth = require('../auth')
 
 // Preload article
 router.param('article', function(req, res, next, slug){
-    Article.findOne({ slug: slug })
+    Article.findOne({ title: slug })
         .populate('author')
         .then(function(article){
             if(!article){ return res.sendStatus(404) }
